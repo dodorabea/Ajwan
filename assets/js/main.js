43bd4708dc30f89de701574328892b7d759e4fa3
Doharabea <95958963+Doharabea@users.noreply.github.com>
2025-01-20 21:18:51 +0200

    about

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,38 +114,7 @@
 
 
 
-var counted = 0;
-$(window).scroll(function() {
 
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (counted == 0 && $(window).scrollTop() > oTop) {
-    $('.count').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 10000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    counted = 1;
-  }
-
-});
 })(jQuery);
 const navbarMenu = document.getElementById("menu");
 const burgerMenu = document.getElementById("burger");
